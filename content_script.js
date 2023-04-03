b353540fa35f10aef43051e26ac39910b7757485
zhangruinan <ruinan.zhang@icloud.com>
2023-04-03 13:08:37 +0800

    feat(main): add side screen option

--- content_script.js
+++ content_script.js
@@ -6,14 +6,21 @@ function addCTAButton() {
 
     if (commentButton) {
         const ctaButton = document.createElement('button');
-        ctaButton.textContent = 'visualize';
+        ctaButton.textContent = 'visualize on side';
         // ctaButton.style.position = 'fixed';
         // ctaButton.style.bottom = '20px';
         // ctaButton.style.right = '20px';
         ctaButton.style.marginLeft = '8px';
         ctaButton.style.zIndex = '1000';
-        ctaButton.onclick = () => {
-            window.open(superusappURL, '_blank');
+        ctaButton.onclick = (event) => {
+            // window.open(superusappURL, '_blank');
+            const iframeWrapper = createIFrame(superusappURL);
+            iframeWrapper.id = 'hn-visualizer';
+            document.body.appendChild(iframeWrapper);
+            document.body.style.width = '30vw';
+            document.body.style.overflowX = 'hidden';
+            event.stopPropagation()
+            event.preventDefault()
         };
         commentButton.insertAdjacentElement('afterend', ctaButton);
     }
@@ -21,9 +28,6 @@ function addCTAButton() {
     // document.body.appendChild(ctaButton)
   }
   
-  if (window.location.href.includes('https://news.ycombinator.com/item?id=')) {
-    addCTAButton();
-  }
 
   function addVisualizeButton(commentLink) {
     const hnPostId = commentLink.href.split('id=')[1];
@@ -84,6 +88,14 @@ function createIFrame(url) {
     };
   
     const iframe = document.createElement('iframe');
+
+    // Add load event listener to the iframe
+    iframe.addEventListener('load', () => {
+        iframe.contentWindow.addEventListener('wheel', (event) => {
+        event.stopPropagation();
+        });
+    });
+
     iframe.src = url;
     iframe.style.width = '100%';
     iframe.style.height = '100%';
@@ -92,8 +104,34 @@ function createIFrame(url) {
     iframeWrapper.appendChild(closeButton);
     iframeWrapper.appendChild(toggleFullScreenButton);
     iframeWrapper.appendChild(iframe);
+
+    addResizeHandle(iframeWrapper); // Add this line
   
     return iframeWrapper;
+  }
+
+
+  function updateIFrame(url) {
+    const existingIFrameWrapper = document.getElementById('hn-visualizer');
+    if (existingIFrameWrapper) {
+      const oldIFrame = existingIFrameWrapper.querySelector('iframe');
+      if (oldIFrame) {
+        existingIFrameWrapper.removeChild(oldIFrame);
+      }
+  
+      const newIFrame = document.createElement('iframe');
+      newIFrame.src = url;
+      newIFrame.style.width = '100%';
+      newIFrame.style.height = '100%';
+      newIFrame.style.border = 'none';
+      existingIFrameWrapper.appendChild(newIFrame);
+    } else {
+      const iframeWrapper = createIFrame(url);
+      iframeWrapper.id = 'hn-visualizer';
+      document.body.appendChild(iframeWrapper);
+      document.body.style.width = '30vw';
+      document.body.style.overflowX = 'hidden';
+    }
   }
   
   // ... (rest of the content_script.js code remains the same)
@@ -102,7 +140,16 @@ function createIFrame(url) {
 
   function addSideScreenButton(commentLink) {
     const hnPostId = commentLink.href.split('id=')[1];
-    const superusappURL = `https://www.superusapp.com/share/hn2map?id=${hnPostId}`;
+    const superusappURL = `https://www.superusapp.com/share/hn2map?sr=true&id=${hnPostId}`;
+
+    const sideScreenLink = document.createElement('a');
+    sideScreenLink.href = '#';
+    sideScreenLink.textContent = 'open on side';
+    sideScreenLink.onclick = (event) => {
+        updateIFrame(commentLink.href);
+        const existingIFrameWrapper = document.getElementById('hn-visualizer');
+        existingIFrameWrapper.style.background="#FFFFFF"
+    }
   
     const visualizeLink = document.createElement('a');
     visualizeLink.href = '#';
@@ -116,7 +163,6 @@ function createIFrame(url) {
           if (oldIFrame) {
             existingIFrameWrapper.removeChild(oldIFrame);
           }
-    
           const newIFrame = document.createElement('iframe');
           newIFrame.src = superusappURL;
           newIFrame.style.width = '100%';
@@ -131,21 +177,12 @@ function createIFrame(url) {
           document.body.style.overflowX = 'hidden';
         }
       };
-    // visualizeLink.onclick = (event) => {
-    //   event.preventDefault();
-    //   const existingIFrame = document.getElementById('hn-visualizer');
-    //   if (existingIFrame) {
-    //     existingIFrame.src = superusappURL;
-    //   } else {
-    //     const iframe = createIFrame(superusappURL);
-    //     iframe.id = 'hn-visualizer';
-    //     document.body.appendChild(iframe);
-    //     document.body.style.width = '30vw';
-    //     document.body.style.overflowX = 'hidden';
-    //   }
-    // };
+
     commentLink.insertAdjacentElement('afterend', visualizeLink);
     commentLink.insertAdjacentText('afterend', ' | ');
+    commentLink.insertAdjacentElement('afterend', sideScreenLink);
+    commentLink.insertAdjacentText('afterend', ' | ');
+    
   }
   
   
@@ -158,9 +195,61 @@ function createIFrame(url) {
         }
     });
   }
+
+  function addResizeHandle(iframeWrapper, iframe) {
+    const resizeHandle = document.createElement('div');
+    resizeHandle.style.position = 'absolute';
+    resizeHandle.style.top = '0';
+    resizeHandle.style.left = '-5px';
+    resizeHandle.style.width = '10px';
+    resizeHandle.style.height = '100%';
+    resizeHandle.style.cursor = 'col-resize';
+    resizeHandle.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+    resizeHandle.style.zIndex = '100';
   
+    const resizeMask = document.createElement('div');
+    resizeMask.style.position = 'absolute';
+    resizeMask.style.top = '0';
+    resizeMask.style.left = '0';
+    resizeMask.style.width = '100%';
+    resizeMask.style.height = '100%';
+    resizeMask.style.backgroundColor = 'rgba(0, 0, 0, 0.0)';
+    resizeMask.style.zIndex = '50';
+    resizeMask.style.display = 'none';
+  
+    let isResizing = false;
+  
+    resizeHandle.addEventListener('mousedown', (event) => {
+      isResizing = true;
+      resizeMask.style.display = 'block';
+      event.preventDefault();
+    });
+  
+    document.addEventListener('mousemove', (event) => {
+      if (isResizing) {
+        const newWidth = window.innerWidth - event.clientX;
+        iframeWrapper.style.width = newWidth + 'px';
+        document.body.style.width = (window.innerWidth - newWidth) + 'px';
+      }
+    });
+  
+    document.addEventListener('mouseup', () => {
+      if (isResizing) {
+        isResizing = false;
+        resizeMask.style.display = 'none';
+      }
+    });
+  
+    iframeWrapper.appendChild(resizeHandle);
+    iframeWrapper.appendChild(resizeMask); // Add the transparent mask to the iframeWrapper
+  }
+
   if (window.location.href.includes('https://news.ycombinator.com/')) {
     addVisualizeButtons();
+  }
+
+  if (window.location.href.includes('https://news.ycombinator.com/item?id=')) {
+    addCTAButton();
   }
 
   function preventTwoFingerSwipe(event) {
